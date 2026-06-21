d805dd90a264251003ea374406e49189604d61e8
SillyCookiemonster <meyere3109@masseyhigh.school.nz>
2026-06-22 11:35:56 +1200

    Final working calculator

--- B_01_shape_calculator.py
+++ B_01_shape_calculator.py
@@ -1,23 +1,36 @@
 import math
 import pandas
-from tabulate import tabulate
+from tabulate2 import tabulate
 
 # Functions go here
-def string_check(question, valid_ans_list=('yes', 'no')):
+def string_check(question, valid_ans_list=('yes', 'no'), exit_code=None, check_first=True):
     """Checks that the users enter the full word
     or the first letter of a word from a list of valid responses"""
 
+    # sets error message
     if valid_ans_list == ('yes', 'no'):
-        err = f"Please enter yes / no (y / n)"
+        error = f"Please enter yes / no (y / n)"
     else:
-        err = f"Please choose a valid answer from {valid_ans_list}"
+        # Set error message from the list of valid responses
+        error = f"Please choose a valid answer from these options: "
 
+        # adding each available option to the string in a presentable way
+        # by making it a list that is easier to read not python's variable list
+        for item in valid_ans_list:
+            error += f"{item}, "
+
+        # remove last 2 characters (", ") from the error string
+        error = error.removesuffix(", ")
+
+    # Loop until valid answer
     while True:
 
+        # gets users answer to the question (and making it lowercase to check if its valid easier)
         response = input(question).lower()
 
-        if response == "xxx":
-            return "end"
+        # Exit when user requests to
+        if response == exit_code:
+            return response
 
         for item in valid_ans_list:
 
@@ -26,10 +39,11 @@ def string_check(question, valid_ans_list=('yes', 'no')):
                 return item
 
             # check the response is the first letter
-            if response == item[0]:
+            if check_first and response == item[0]:
                 return item
 
-        print(err)
+        # If it wasn't an option in the list, print error and restart loop
+        print(error)
 
 
 def make_statement(statement, decoration, amount=3):
@@ -40,14 +54,16 @@ def make_statement(statement, decoration, amount=3):
 
 
 def instructions():
+    """prints instructions for how to use the calculator"""
+
     print()
     make_statement("Instructions", "ℹ️")
 
     print('''
 This program will..
 
-- Ask for he basic 2D shape you want to calculate 
-    the perimeter and area for. The valid shapes are 
+- Ask for a basic 2D shape you want to calculate 
+    the perimeter and area for. The valid shapes are: 
     square, rectangle, circle, and triangle
     (Tip! You only need to enter the first letter
     of a shape to enter it).
@@ -59,13 +75,17 @@ This program will..
 
 - Output the area and perimeter of the shapes 
     along with the given information in a table.
+    
+Once you have finished using the calculator, 
+  enter "xxx" during shape selection.
 
     ''')
 
 
-def num_check(question, num_type, exit_code=None):
+def num_check(question, num_type="float", exit_code=None):
     """Checks the user enters a number above 0"""
 
+    # sets error message for what requirements the user is required to meet (integer or float)
     if num_type == "int":
         error = "Error. Please enter an integer above 0."
         change_to = int
@@ -73,32 +93,43 @@ def num_check(question, num_type, exit_code=None):
         error = "Error. Please enter a number above 0."
         change_to = float
 
+    # Loops to make sure a number is indeed entered
     while True:
+        # Ask user for number using the question
         response = input(question)
 
+        # Check if user requests exit and exit if so
         if response == exit_code:
             return response
 
         try:
+            # Check if the response can be changed into the expected answer format
             response = change_to(response)
+
+            # Check if the user enters a valid length
+            # Lengths must always be larger than zero
             if response > 0:
+                # Return when given answer passed all checks and is valid
                 return response
+
+            # Answer isn't a valid length even if in correct format (is not larger than zero)
             else:
                 print(error)
 
+        # If converting answer format fails, print error and loop again
         except ValueError:
             print(error)
 
 
 def rectangle_calculator(square_or_rectangle):
-    """Gets the perimeter and area of a rectangle"""
+    """Gets the perimeter and area of a rectangle using given measurements."""
 
     # Ask for length
-    length = num_check("Length: ", "float")
+    length = num_check("Length: ")
 
     # get height of non-square rectangle
     if square_or_rectangle == "rectangle":
-        height = num_check("Height: ", "float")
+        height = num_check("Height: ")
 
     # set height to length when shape is square
     else:
@@ -114,9 +145,9 @@ def rectangle_calculator(square_or_rectangle):
 
 
 def circle_calculator():
-    """Gets the perimeter and area of a circle"""
+    """Gets the perimeter and area of a circle using given measurements."""
 
-    radius = num_check("Radius: ", "float")
+    radius = num_check("Radius: ")
 
     # calculate diameter, perimeter and area  incl. 2 dp
     diameter = 2*radius
@@ -129,38 +160,47 @@ def circle_calculator():
 
 
 def triangle_calculator():
-    """Gets the perimeter and area of a triangle,
-    reliant on which type of triangle is chosen"""
+    """Gets the perimeter and area of a triangle using the given measurements,
+    including which type of triangle it is (if base & height are known or the side lengths)."""
 
     # Get which lengths are known (Lengths of the base & height or the sides)
-    known_lengths = string_check("Side lengths or the base and height lengths? ",
+    known_lengths = string_check("Do you have the side lengths or the base and height lengths? ",
                                  ('sides', 'base and height'))
 
     # calculations if sides are known
     if known_lengths == "sides":
 
         # Get side lengths
-        side_1 = num_check("First side: ", "float")
-        side_2 = num_check("Second side: ", "float")
-        side_3 = num_check("Third side: ", "float")
+        side_1 = num_check("First side: ")
+        side_2 = num_check("Second side: ")
+        side_3 = num_check("Third side: ")
 
         base_or_sides = f"{side_1}, {side_2}, {side_3}"
+
+        # Checks if the triangle is real and returning if not
+        if (side_1+side_2<=side_3) or (side_2+side_3<=side_1) or (side_3+side_1<=side_2):
+            return "not real"
+
+        # Height is incalculable
         height = "N/A"
+
+        # Calculate perimeter from side lengths
         perimeter = side_1+side_2+side_3
 
-        # implement herons law
+        # implement herons law to find area
         semi_perimeter = perimeter / 2
-        # sqrt(s*(s - a)*(s - b)*(s - c)) inc. 2 dp
+        # where s = half of the perimeter
+        # sqrt(s*(s - a)*(s - b)*(s - c))   |   incl. 2 dp
         area_unrooted = (semi_perimeter * (semi_perimeter - side_1) * (semi_perimeter - side_2)
                          * (semi_perimeter - side_3))
-        area = f"√{area_unrooted} / {math.sqrt(area_unrooted):.2f}"
+        area = f"√{area_unrooted:.2f} / {math.sqrt(area_unrooted):.2f}"
 
     # calculations if base and height lengths are known
     else:
 
         # get length of the base and height
-        base_or_sides = num_check("Base: ", "float")
-        height = num_check("Height: ", "float")
+        base_or_sides = num_check("Base length: ")
+        height = num_check("Height: ")
 
         # calculate perimeter and area
         perimeter = "N/A"
@@ -175,37 +215,38 @@ def triangle_calculator():
 
 # Title and instructions
 
-make_statement("2D Shape Calculator", "🔷🔶🔷", 1)
+make_statement("Ezra's 2D Shape Calculator", "🔷🔶🔷", 1)
 
 print()
 want_instructions = string_check("Do you want to see the instructions? ")
 
+# Ask if users want to know how to use the calculator
 if want_instructions == "yes":
     instructions()
 
-# lists for panda
+# Setup lists to be used for data storage to be put into Pandas
 all_shapes = []
 all_base = []
 all_height = []
 all_perimeter = []
 all_area = []
 
-# Panda dictionary
+# Dictionary for Pandas
 shape_dict = {
     "Shape" : all_shapes,
-    "Side Length(s) /\nDiameter" : all_base,
-    "Height /\nRadius" : all_height,
+    "Side Length(s) /\nRadius" : all_base,
+    "Height /\nDiameter" : all_height,
     "Perimeter" : all_perimeter,
     "Area" : all_area
 }
 
-# Loop until exit
+# Loop until users request exit
 while True:
     print()
 
-    # Get shapes
+    # Get the shape the user wants to calculate for
     shape = string_check("What shape do you want to calculate? ",
-                         ('square', 'rectangle', 'circle', 'triangle'))
+                         ('square', 'rectangle', 'circle', 'triangle'), "xxx")
 
     # run shape calculations per each shape
     if shape == "square":
@@ -216,6 +257,9 @@ while True:
         data = circle_calculator()
     elif shape == "triangle":
         data = triangle_calculator()
+        if data == "not real":
+            print("This triangle is not real (side lengths can't form a triangle).")
+            continue
         # Change shape title depending on which type of triangle
         if data[4] == "sides":
             shape += " (sides)"
@@ -226,17 +270,31 @@ while True:
     else:
         break
 
-    # storing shape in list
+    units = string_check("What unit of measurement are you using for this shape? ", ("mm", "cm", "m", "km"), None, False)
+
+    # storing shape in list for data to be output in Pandas
     all_shapes.append(shape.title())
-    all_base.append(data[0])
-    all_height.append(data[1])
-    all_perimeter.append(data[2])
-    all_area.append(data[3])
+    all_base.append(f"{data[0]:.2f} {units}")
+    all_height.append(f"{data[1]:.2f} {units}")
+    all_perimeter.append(f"{data[2]:.2f} {units}")
+    all_area.append(f"{data[3]:.2f} {units}²")
 
-# Make pandas
-shape_frame = pandas.DataFrame(shape_dict)
+# If the user didn't enter any shapes there is no need to print a table
+if len(all_shapes)  == 0:
+    print()
+    print("You didn't enter any shapes to calculate")
 
-table_string = tabulate(shape_frame, headers='keys', tablefmt='psql', showindex=False,
-                         colalign=("left", "right", "right", "right", "right"), headersglobalalign= 'left')
+else:
+    # Form Pandas with the data
+    shape_frame = pandas.DataFrame(shape_dict)
 
-print(table_string)
+    # Representing the Pandas as a table with customised layout to make it look tidy
+    table_string = tabulate(shape_frame, headers='keys', tablefmt='rounded_grid', showindex=False,
+                             colalign=("l", "r", "r", "r", "r"), headersglobalalign="right")
+
+    print()
+    make_statement("Calculations (With Units to 2 Decimal Places)", "=", 5)
+    print(table_string)
+
+print()
+make_statement("Thank you for using Ezra's 2D Shape Calculator!", "🔶🔷🔶", 1)
